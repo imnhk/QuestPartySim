7ad124d5a339cfcc51a036def3165f1bd9bcbe6a
Nam-hyeon Kim <koloke1712@gmail.com>
2020-01-08 17:28:17 +0900

    Initial commit

---
+++ Assets/ThrowGrabbed.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGrabbed : MonoBehaviour
+{
+    public OVRInput.Button throwButton;
+    public float throwForce;
+    public Vector3 angularVelocity;
+    
+    private OVRGrabbable ovrGrabbable;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ovrGrabbable.isGrabbed && OVRInput.GetDown(throwButton, ovrGrabbable.grabbedBy.Controller))
+        {
+            Throw();
+        }
+    }
+
+    private void Throw()
+    {
+        Quaternion throwDirection = ovrGrabbable.grabbedBy.throwLine.rotation;
+
+        ovrGrabbable.grabbedBy.ForceRelease(ovrGrabbable);
+        ovrGrabbable.GrabEnd(throwDirection * Vector3.forward * throwForce, angularVelocity);
+    }
+}
