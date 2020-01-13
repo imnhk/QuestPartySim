bac92ccc9ee4a80ee5b9ce8b31c0cc1e632d9f03
Nam-hyeon Kim <koloke1712@gmail.com>
2020-01-13 16:50:59 +0900

    Add Timer, Object Health

---
+++ Assets/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    Timer timer;
+    public GameObject timerText;
+
+    void Start()
+    {
+        timer = new Timer(30, timerText);
+        timer.Start();
+    }
+
+    void Update()
+    {
+        timer.Update();
+    }
+}
+
+public class Timer
+{
+    float startTime;
+    float leftTime;
+    bool isActive;
+    TextMeshProUGUI timerText;
+
+    public Timer(float startTime, GameObject timerText)
+    {
+        this.startTime = startTime;
+        this.timerText = timerText.GetComponent<TextMeshProUGUI>();
+        Reset();
+    }
+
+    public bool IsOver{ get { return leftTime <= 0; } }
+
+    public void Update()
+    {
+        if (isActive == false)
+            return;
+
+        leftTime -= Time.deltaTime;
+        timerText.text = leftTime.ToString("F1");
+
+        if (IsOver)
+            isActive = false;
+    }
+
+    public void Reset()
+    {
+        leftTime = startTime;
+        isActive = false;
+        timerText.text = leftTime.ToString("F1");
+    }
+
+    public void Start()
+    {
+        if (IsOver)
+            return;
+        isActive = true;
+    }
+
+    public void Stop()
+    {
+        isActive = false;
+    }
+        
+}
---
+++ Assets/Scripts/ObjectHealth.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class ObjectHealth : MonoBehaviour
+{
+    public int MaxHp;
+
+    Material material;
+    Color startColor;
+    int hp;
+    float lastCollisionTime;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+        startColor = material.color;
+    }
+
+    void Start()
+    {
+        Assert.IsTrue(MaxHp > 0);
+        hp = MaxHp;
+        lastCollisionTime = Time.time;
+    }
+
+    void Update()
+    {
+        if(hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (Time.time < lastCollisionTime + 0.1f)
+            return;
+        lastCollisionTime = Time.time;
+
+        float impulse;
+        if (collision.rigidbody != null)
+            impulse = collision.rigidbody.mass * collision.relativeVelocity.magnitude;
+        else
+            impulse = GetComponent<Rigidbody>().mass * collision.relativeVelocity.magnitude;
+
+        if (impulse > 3)
+        {
+            Debug.Log("impulse " + impulse);
+            hp -= (int)impulse * 2;
+            material.color = Color.Lerp(Color.black, startColor, (float)hp / MaxHp);
+        }
+    }
+}
