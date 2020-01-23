9c49e2d48b9841e4dabd3ceacc2f799d9684cf77
Nam-hyeon Kim <koloke1712@gmail.com>
2020-01-23 14:53:42 +0900

    Add food object interaction

---
+++ Assets/Edible.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edible : MonoBehaviour
+{
+
+    [SerializeField]
+    private int size = 3;
+    private int leftCount;
+
+    private Vector3 defaultScale;
+
+    private float eatDelay = 1f;
+    private float lastEatTime = 0;
+
+    private void Start()
+    {
+        leftCount = size;
+        defaultScale = transform.localScale;
+    }
+
+    public void Eaten()
+    {
+        if (Time.time < lastEatTime + eatDelay)
+            return;
+        lastEatTime = Time.time;
+
+        leftCount -= 1;
+        // Debug.Log("Eaten! size " + leftCount);
+
+        if(leftCount <= 0)
+            Destroy(this.gameObject);
+
+        transform.localScale = defaultScale * ((float)leftCount / size);
+
+    }
+}
---
+++ Assets/Mouth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouth : MonoBehaviour
+{
+    AudioSource mouthAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouthAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Edible food = other.gameObject.GetComponent<Edible>();
+        if (food)
+        {
+            food.Eaten();
+        }
+    }
+
+}
