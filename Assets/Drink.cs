a6c30b6f9f7b33e6a07e8f8d56bdb732496d6468
Nam-hyeon Kim <koloke1712@gmail.com>
2020-02-07 16:47:37 +0900

    Add drinks particle

--- Assets/Drink.cs
+++ Assets/Drink.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class Drink : MonoBehaviour
 {
-    public float leftTime = 5f;
+    public float leftTime = 8f;
 
     private ParticleSystem particle;
+
     void Awake()
     {
         particle = GetComponentInChildren<ParticleSystem>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float angle = Vector3.Angle(Vector3.up, transform.rotation.eulerAngles);
+        float angle = Vector3.Angle(Vector3.up, transform.up);
+        // transform.Rotate(new Vector3(0.5f, 0, 0));
+
         if(angle > 85 && leftTime > 0)
         {
-            particle.Play();
             leftTime -= Time.fixedDeltaTime;
 
+            if (!particle.isPlaying)
+            {
+                particle.Play();
+            }
         }
         else
         {
-            particle.Stop();
+            if(particle.isPlaying)
+                particle.Stop();
         }
     }
 }
