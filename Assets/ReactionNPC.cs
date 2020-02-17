ed8b20fab7f121076cc647ad6d99fdf869046cc3
Nam-hyeon Kim <koloke1712@gmail.com>
2020-02-17 11:59:36 +0900

    Add room2 props

--- Assets/ReactionNPC.cs
+++ Assets/ReactionNPC.cs
@@ -13,43 +13,20 @@ public class ReactionNPC : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    void Start()
+    public void PartCollision(PART part, float impulse)
     {
-        
-    }
 
-    private void Update()
-    {
-        
-    }
-
-    // 작은 충격 n회 반복 시 플레이어 추격, 잡히면 게임오버.
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        float impulse = GetImpulse(collision);
-        Debug.LogError("IMPULSE: " + impulse);
-
-        // 작은 충격시 표정 변화, 플레이어쪽으로 방향 전환
+        // 작은 충격 n회 반복 시 플레이어 추격, 잡히면 게임오버.
         if(impulse > 1f)
         {
 
         }
-        // 큰 충격시 Animator 해제, 게임오버
-        else if (impulse > 3f)
+        // 머리에 큰 충격시 Animator 해제, 게임오버
+        if (part == PART.HEAD && impulse > 5f)
         {
             animator.enabled = false;
             GameManager.Instance.GameOver(GameManager.GAMEOVER.KICKED_OUT);
         }
-    }
-
-    float GetImpulse(Collision coll)
-    {
-        if (coll.rigidbody != null)
-            return coll.rigidbody.mass * coll.rigidbody.velocity.magnitude;
-        else
-            return 0;
     }
 
 }
